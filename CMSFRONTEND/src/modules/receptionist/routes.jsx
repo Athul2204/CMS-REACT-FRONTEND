1d0c25a169c577c5f5563d66806af84bb24d6f69
Sujith <sujith@gmail.com>
2026-03-28 11:47:59 +0530

    restored src folder

--- CMSFRONTEND/src/modules/receptionist/routes.jsx
+++ CMSFRONTEND/src/modules/receptionist/routes.jsx
@@ -1,10 +1,19 @@
-import ReceptionistDashboard from "./pages/ReceptionistDashboard";
+import { Routes, Route } from "react-router-dom";
 
-const receptionistRoutes = [
-  {
-    path: "/reception/dashboard",
-    element: <ReceptionistDashboard />,
-  },
-];
+import ReceptionDashboard from "./pages/ReceptionDashboard";
+import PatientList from "./pages/PatientList";
+import AddPatient from "./pages/AddPatient";
+import DoctorAvailability from "./pages/DoctorAvailability";
 
-export default receptionistRoutes;
+const ReceptionRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<ReceptionDashboard />} />
+      <Route path="patients" element={<PatientList />} />
+      <Route path="add-patient" element={<AddPatient />} />
+      <Route path="availability" element={<DoctorAvailability />} />
+    </Routes>
+  );
+};
+
+export default ReceptionRoutes;
